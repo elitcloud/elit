0f7086636f0cf1486fd80390fa6f6bc2564125f2
tlee54 <leetdh1028@gmail.com>
2017-02-17 12:03:03 -0500

    model unittest prototype

--- python/elit/test_model.py
+++ python/elit/test_model.py
@@ -14,14 +14,46 @@
 # limitations under the License.
 # ========================================================================
 import unittest
+from model import LabelMap
+
 __author__ = 'Jinho D. Choi'
 
-
 class LabelMapTest(unittest.TestCase):
-    def test(self):
-        # TODO:
-        pass
+	#checking str add to LabelMap
+	def test_str_str(self):
+		the_map = LabelMap()
+		self.assertEqual(the_map.__str__(), '[]')
+		the_map.add("hello")
+		self.assertEqual(the_map.__str__(), "[\'hello\']")
+
+	#checking integer add to LabelMap
+	def test_str_int(self):
+		the_map = LabelMap()
+		the_map.add(1)
+		self.assertEqual(the_map.__str__(), '[\'1\']')
+
+	def test_len(self):
+		the_map = LabelMap()
+		self.assertEqual(the_map.__len__(), 0)
+		for item in range(10):
+			the_map.add(item)
+		self.assertEqual(the_map.__len__(), 10)
+
+	def test_index(self):
+		the_map = LabelMap()
+		for item in range(10):
+			the_map.add(item)
+		for item in range(10):
+			self.assertEqual(the_map.index(item), int(item))
+
+	def test_add(self):
+		the_map = LabelMap()
+		self.assertEqual(the_map.add('a'), 0)
+		self.assertEqual(the_map.add('b'), 1)
+		self.assertEqual(the_map.add('a'), 0)
+		self.assertEqual(the_map.add('b'), 1)
+		self.assertEqual(the_map.add(1), 2)
 
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
