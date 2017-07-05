8c6b2d6db05fc747c27fc4643d4c4b0effb94ab0
Gary Lai <garylai1990@gmail.com>
2017-07-05 10:40:20 -0400

    add dependency

--- setup.py
+++ setup.py
@@ -10,7 +10,14 @@ config = {
     'download_url': 'git@github.com:emorynlp/elit.git',
     'author_email': 'choi@mathcs.emory.edu',
     'version': '0.1',
-    'install_requires': ['nose'],
+    'install_requires': [
+        'nose',
+        'Cython>=0.25',
+        'mxnet>=0.9.4',
+        'argparse>=1.4',
+        'gensim>=1',
+        'fasttext>=0.8'
+        ],
     'packages': ['elit'],
     'scripts': [],
     'name': 'elit'
