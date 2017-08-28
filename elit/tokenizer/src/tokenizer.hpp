94e098398e15989e2045495ebb3862e700cb24fc
Gary Lai <garylai1990@gmail.com>
2017-08-28 15:06:58 -0400

    remove src

--- elit/tokenizer/src/tokenizer.hpp
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <vector>
-#include <string>
-
-char * tokenize(char * name);
-std::vector<std::string> vectorize(std::string s);
