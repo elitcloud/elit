edb1f2dc771c0698521edbae27a9dd52c6290197
Gary Lai <garylai1990@gmail.com>
2017-09-07 14:04:37 -0400

    remove unused file[ci skip]

--- rsync.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rsync -avz --delete /Users/tlee/Desktop/elit -e ssh timothy@ainos03.mathcs.emory.edu:
--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ Operating System :: MacOS
 MAJOR = 0
 MINOR = 1
 MICRO = 5
-ISRELEASED = True
+ISRELEASED = False
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 
