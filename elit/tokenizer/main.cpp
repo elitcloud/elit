8fd6214b39ce68634eacf94622288fcca8d82e7f
Gary Lai <garylai1990@gmail.com>
2017-09-12 23:08:21 -0400

    Set RESOURCE_ROOT

--- elit/tokenizer/main.cpp
+++ elit/tokenizer/main.cpp
@@ -30,34 +30,19 @@ void print_v(wstring s)
 int main(int argc, const char * argv[])
 {
     wstring t[] = {
-            L"",
-            L"  ",
-            L"AB CD EF",
-            L"  AB CD EF",
-            L"AB CD EF  ",
-            L"  AB CD EF  ",
-            L"AB  CD  EF",
-            L"ab&larr;cd&#8592;&#x2190;ef&#X2190;",
-            L"http://ab cd.ftp://ef eftp",
-            L"jinho@elit.com,jinho.choi@elit.com,choi@elit.emory.edu,jinho:choi@0.0.0.0",
-            L"#happy2018,@Jinho_Choi: ab@cde",
-            L":-) A:-( :). B:smile::sad: C:):(! :)., :)",
-            L"[a](1)(1.a)[11.22.a.33](A.1)[a1]",
-            L"1997-2012,1990's-2000S '97-2012 '12-14's '90s '900",
-            L"1997,1998/1999/2000",
-            L"aa;;;b:c\"\"d\"\"",
-            L"don't he's can't does'nt 0's DON'T",
-            L"0'''s ''a''a'' 'a'a'a'",
-            L"a,b,c,d,",
-            L"0.12 .34 +1 ",
-            L"#1. $1,000-2K.?!'''...",
-            L"\"w.r.t. this. Ph.D. Ph.D.\"",
-            L"inter-state, far-most, inter - state, far - most",
-            L"w/o ab/cd AB/CD",
-            L"No. 1 No. You!",
-            L"11:30pm 3:0 a:b ab:cd",
-            L"20mg 100cm 11a.m. 10PM",
-            L"whadya DON'CHA",
+            L"He possesses  dollars!"
+        L"It is simply papier-mâché", //unresolved!
+            L"The antique clock is from c.1900!", //unresolved!
+            L"Have you received my C.V.?",//unresolved
+            L"Ling et al. shall not get their way!",
+            L"Broccoli, etc. will not be a problem.",
+            L"Ibid., 29", //fixed
+            L"RC J.D. is a good man!",
+            L"I need 1 lb. of food!", //fixed
+            L"One equals three, Q.E.D.!", //unresolved
+            L"R.I.P., Mr. Finley!"
+
+
     };
 
     for (auto s : t) print_v(s);
