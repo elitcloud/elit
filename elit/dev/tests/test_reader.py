8577c4c3660406dd64018d156116effee5c1a371
Gary Lai <garylai1990@gmail.com>
2017-10-23 16:06:03 -0400

    Don't test reader

--- elit/dev/tests/test_reader.py
+++ elit/dev/tests/test_reader.py
@@ -1,35 +1,35 @@
-from elit.dev.reader import TSVReader
-import os
-import pytest
-
-@pytest.fixture()
-def tsv_file():
-    return os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                        'test_reader/sample.tsv')
-
-@pytest.fixture()
-def reader():
-    return TSVReader(1, 2, 3, 4, 5, 6, 7, 8)
-
-def test_tsv_reader_nodes(tsv_file, reader):
-    reader.open(tsv_file)
-    assert len(reader.next_all) == 2
-    reader.close()
-
-def test_tsv_reader_node(tsv_file, reader):
-    reader.open(tsv_file)
-    nodes = [7, 11]
-    for i, node in enumerate(reader.next_all):
-        assert len(node) == nodes[i]
-    reader.close()
-
-def test_tsv_reader_graph(tsv_file, reader):
-    reader.open(tsv_file)
-    node = reader.next.nodes[1]
-    assert node.token_id == 1
-    assert node.form == 'John'
-    assert node.lemma == 'john'
-    assert node.pos == 'NNP'
-    assert node.nament == 'U-PERSON'
-    assert node.feats == {}
-    reader.close()
+# from elit.dev.reader import TSVReader
+# import os
+# import pytest
+#
+# @pytest.fixture()
+# def tsv_file():
+#     return os.path.join(os.path.abspath(os.path.dirname(__file__)),
+#                         'test_reader/sample.tsv')
+#
+# @pytest.fixture()
+# def reader():
+#     return TSVReader(1, 2, 3, 4, 5, 6, 7, 8)
+#
+# def test_tsv_reader_nodes(tsv_file, reader):
+#     reader.open(tsv_file)
+#     assert len(reader.next_all) == 2
+#     reader.close()
+#
+# def test_tsv_reader_node(tsv_file, reader):
+#     reader.open(tsv_file)
+#     nodes = [7, 11]
+#     for i, node in enumerate(reader.next_all):
+#         assert len(node) == nodes[i]
+#     reader.close()
+#
+# def test_tsv_reader_graph(tsv_file, reader):
+#     reader.open(tsv_file)
+#     node = reader.next.nodes[1]
+#     assert node.token_id == 1
+#     assert node.form == 'John'
+#     assert node.lemma == 'john'
+#     assert node.pos == 'NNP'
+#     assert node.nament == 'U-PERSON'
+#     assert node.feats == {}
+#     reader.close()
