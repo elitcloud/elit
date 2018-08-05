da29efe0758c1938e58cd2121b9dc4c9b117bb95
Gary Lai <garylai1990@gmail.com>
2018-08-05 00:20:08 -0400

    :construction: (file) mv json and tsv reader to utils

--- elit/token_tagger.py
+++ elit/token_tagger.py
@@ -422,64 +422,6 @@ class TokenSequenceTagger(TokenTagger, SequenceComponent):
         :return: the list of sequence or batch states corresponding to the input documents.
         """
         return group_states(documents, lambda d: TokenSequenceTaggerState(d, self.key, self.label_map, self.vsm_list, self.feature_windows, self.padout))
-
-
-# ======================================== Command-Line ========================================
-
-def tsv_reader(filepath, key, args):
-    documents = []
-    wc = sc = 0
-
-    for filename in glob.glob(filepath):
-        sentences, tokens, tags = [], [], []
-        fin = open(filename)
-
-        for line in fin:
-            if line.startswith('#'):
-                continue
-            l = line.split()
-
-            if l:
-                tokens.append(l[args.tok])
-                tags.append(l[args.tag])
-            elif len(tokens) > 0:
-                wc += len(tokens)
-                sentences.append(Sentence({TOK: tokens, to_gold(key): tags}))
-                tokens, tags = [], []
-
-        if len(tokens) > 0:
-            wc += len(tokens)
-            sentences.append(Sentence({TOK: tokens, to_gold(key): tags}))
-
-        fin.close()
-        sc += len(sentences)
-        documents.append(Document(sen=sentences))
-
-    print('Reading: dc = %d, sc = %d, wc = %d' % (len(documents), sc, wc))
-    return documents
-
-
-def json_reader(filepath) -> List[Document]:
-    # TODO: to be filled
-    documents = []
-    dc = wc = sc = 0
-
-    for filename in glob.glob('{}/*.json'.format(filepath)):
-        assert filename.endswith('.json')
-        with open(filename) as f:
-            docs = json.load(f)
-            for doc in docs:
-                sentences = []
-                for sen in doc['sen']:
-                    wc += len(sen['tok'])
-                    sentences.append(Sentence(sen))
-                sc += len(sentences)
-                documents.append(Document(sen=sentences))
-            dc += len(documents)
-    print('Reading: dc = %d, sc = %d, wc = %d' % (dc, sc, wc))
-    return documents
-
-
 def reader_args(s):
     """
     :param s: (tsv|json)(;\\d:\\d)*
