73faf99711f113c3844d950ee141808eb9448038
Jinho Choi <jinho.choi@emory.edu>
2018-11-16 12:06:32 -0500

    Updated docs.

---
+++ demo/elit_demo.py
@@ -0,0 +1,31 @@
+# ========================================================================
+# Copyright 2018 Emory University
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ========================================================================
+from elit.nlp.morph_analyzer import EnglishMorphAnalyzer
+from elit.structure import Document, Sentence, MOR, TOK, POS
+
+__author__ = 'Jinho D. Choi'
+
+
+
+m = EnglishMorphAnalyzer()
+
+doc = Document()
+sen = Sentence()
+sen[TOK] = ["Jinho", "Choi", "is", "a", "professor", "at", "Emory", "University", "."]
+sen[POS] = ["NNP", "NNP", "VBZ", "DT", "NN", "IN", "NNP", "NNP", "."]
+doc.add_sentence(sen)
+m.decode([doc])
+print(sen[MOR])
