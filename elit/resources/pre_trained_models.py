63de891a1885d40fcda597862009de21f062f5fc
hankcs <jfservice@126.com>
2019-06-19 21:31:24 -0400

    fix lm url

--- elit/resources/pre_trained_models.py
+++ elit/resources/pre_trained_models.py
@@ -4,7 +4,7 @@
 
 RESOURCE_URL_PREFIX = 'https://s3-us-west-2.amazonaws.com/elitcloud-public-data/'
 LM_NEWS_FORWARD = RESOURCE_URL_PREFIX + 'language-model/lm-news-forward.zip'
-LM_NEWS_BACKWARD = RESOURCE_URL_PREFIX + 'language-model/lm-news-forward.zip'
+LM_NEWS_BACKWARD = RESOURCE_URL_PREFIX + 'language-model/lm-news-backward.zip'
 POS_JUMBO = RESOURCE_URL_PREFIX + 'pos/jumbo-pos.zip'
 NER_JUMBO = RESOURCE_URL_PREFIX + 'ner/jumbo-ner.zip'
 DEP_JUMBO = RESOURCE_URL_PREFIX + 'dep/jumbo-dep.zip'
