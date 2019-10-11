3f886b1ae1c1f62ed961d8adf51c4245c4807666
hankcs <jfservice@126.com>
2019-10-11 13:03:24 -0400

    pos-gold

--- elit/tests/demo/demo_cnn_pos.py
+++ elit/tests/demo/demo_cnn_pos.py
@@ -1,10 +1,20 @@
 # -*- coding:utf-8 -*-
 # Author: hankcs
 # Date: 2019-10-03 16:27
+from types import SimpleNamespace
+
 from elit.component.embedding.fasttext import FastText
-from elit.component.token_tagger import CNNTokenTagger
+from elit.component.tagger.corpus import conll_to_documents, label_map_from_conll
+from elit.component.token_tagger.cnn import CNNTokenTagger
 from elit.util.mx import mxnet_prefer_gpu
 
-# https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.en.300.bin.gz
+label_map = label_map_from_conll('data/ptb/pos/train.tsv')
+print(label_map)
 tagger = CNNTokenTagger(ctx=mxnet_prefer_gpu(), key='pos',
-                        embs=[FastText('https://elit-models.s3-us-west-2.amazonaws.com/fasttext.debug.bin.zip')])
+                        embs=[FastText('https://elit-models.s3-us-west-2.amazonaws.com/fasttext.debug.bin.zip')],
+                        input_config=SimpleNamespace(row='what?', col='why?', dropout=0.5),
+                        output_config=SimpleNamespace(num_class=12, flatten=True),
+                        label_map=label_map
+                        )
+tagger.train(conll_to_documents('data/conll-03/debug/eng.trn'), conll_to_documents('data/conll-03/debug/eng.dev'),
+             'data/model/cnntagger')
