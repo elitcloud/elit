a310bae9630a941aa1c1e0f96e03277ebba1dbd2
hankcs <jfservice@126.com>
2020-03-11 15:47:10 -0400

    EnglishLemmatizer demo

--- elit/tests/demo/demo_lemma.py
+++ elit/tests/demo/demo_lemma.py
@@ -1,6 +1,19 @@
 # -*- coding:utf-8 -*-
 # Author: hankcs
 # Date: 2019-11-02 12:16
+from elit.resources.pre_trained_models import ELIT_POS_FLAIR_EN_MIXED
+
+from elit.component import EnglishTokenizer, POSFlairTagger
+from elit.structure import Document
+
 from elit.component.lemmatizer import EnglishLemmatizer
 
-EnglishLemmatizer()
+tagger = POSFlairTagger()
+model_path = ELIT_POS_FLAIR_EN_MIXED
+tagger.load(model_path)
+components = [EnglishTokenizer(), tagger, EnglishLemmatizer()]
+docs = ['Sentence one. Sentence two.']
+for c in components:
+    docs = c.decode(docs)
+for d in docs:  # type: Document
+    print(d)
